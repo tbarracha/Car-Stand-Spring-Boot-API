fe4f8452f285886f691ee9b3602da8fb369be106
tbarracha <ti.barracha@gmail.com>
2024-02-16 11:27:35 +0000

    Revert "Implemented CarBrand API"
    This reverts commit 7870fa0332673520bf74e249a6c77286ad1ec4fd.

--- src/main/java/pt/carstand/CarStand/Models/Interfaces/IIdentifiable.java
+++
@@ -1,5 +0,0 @@
-package pt.carstand.CarStand.Models.Interfaces;
-
-public interface IIdentifiable<Key> {
-    Key getId();
-}
--- src/main/java/pt/carstand/CarStand/Models/Interfaces/INameable.java
+++
@@ -1,5 +0,0 @@
-package pt.carstand.CarStand.Models.Interfaces;
-
-public interface INameable {
-    String getName();
-}
