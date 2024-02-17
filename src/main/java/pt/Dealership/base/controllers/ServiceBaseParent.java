11da49ffb0e1efd9d7a005021996c80cd4bff420
tbarracha <ti.barracha@gmail.com>
2024-02-17 12:03:08 +0000

    added java doc

--- src/main/java/pt/Dealership/base/controllers/ServiceBaseParent.java
+++ src/main/java/pt/Dealership/base/controllers/ServiceBaseParent.java
@@ -3,6 +3,11 @@ package pt.Dealership.base.controllers;
 import org.springframework.hateoas.RepresentationModel;
 import pt.Dealership.base.interfaces.ICrud;
 
+/**
+ * Parent class for ServiceBase, so we can reference this as type in the ControllerBase
+ * @param <T> model type (ex: brand, car, etc)
+ * @param <Key> primary key
+ */
 public abstract class ServiceBaseParent<T extends RepresentationModel<T>, Key>  implements ICrud<T, Key> {
 
 }
